aca70ccfe84d3a86c3f5a253e5c991a4f4434eb3
KangLin <kl222@126.com>
2016-09-12 20:19:28 +0800

    add code

--- main.cpp
+++ main.cpp
@@ -1,11 +1,35 @@
 #include "mainwindow.h"
 #include <QApplication>
 
+#include <osg/Notify>
+#include <osgViewer/CompositeViewer>
+#include <osgGA/CameraManipulator>
+#include <osg/Node>
+#include <osgEarthUtil/ExampleResources>
+#include <osgEarthUtil/EarthManipulator>
+#include <osgEarthQt/ViewerWidget>
+
+#include <QDebug>
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+
+    // load an earth file
+    osg::Node* node = osgDB::readNodeFile("C:/SXEarth2.6.5/bin/google.earth");
+    if ( !node )
+        qDebug() << "Failed to load earth file." ;
+/*
+    osgViewer::Viewer viewer;
+    viewer.setThreadingModel( viewer.SingleThreaded );
+    viewer.setRunFrameScheme( viewer.ON_DEMAND );
+    viewer.setCameraManipulator( new osgEarth::Util::EarthManipulator() );
+    viewer.setSceneData( node );
+    QWidget* viewerWidget = new osgEarth::QtGui::ViewerWidget( &viewer );
+    w.setCentralWidget(viewerWidget);
+ */
     
     return a.exec();
 }
--- pri/ThirdLibrary.pri
+++ pri/ThirdLibrary.pri
@@ -3,3 +3,6 @@
 myPackagesExist(openscenegraph) {
     MYPKGCONFIG *= openscenegraph
 }
+
+LIBS += -losgEarthQt -losgEarthUtil -losgEarthSplat -losgEarthSymbology -losgEarthFeatures -losgEarthAnnotation -losgEarth 
+#LIBS +=  -losg -losgDB -losgFX -losgGA -losgParticle -losgSim -losgText -losgUtil -losgTerrain -losgManipulator -losgViewer -losgWidget -losgShadow -losgAnimation -losgVolume -lOpenThreads
