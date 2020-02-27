ab0160693eb16fb42eb1626ed5129dea80652770
KangLin <kl222@126.com>
2020-02-27 16:07:21 +0800

    Modify qt-installer.sh

--- ci/qt-installer.sh
+++ ci/qt-installer.sh
@@ -2,13 +2,22 @@
 
 #http://stackoverflow.com/questions/25105269/silent-install-qt-run-installer-on-ubuntu-server
 #http://doc.qt.io/qtinstallerframework/noninteractive.html
+#参考：https://github.com/benlau/qtci
+#     https://github.com/rabits/dockerfiles
+#     https://github.com/mjscosta/qt-silent-installer
+
 set -e #quit on error
 
 if [ $# -lt 2 ];
 then
-    echo qt-installer.sh qt-installer output_path
+    echo qt-installer.sh qt-installer-file output_path
     exit -1
 fi
+
+function version_gt() { test "$(echo "$@" | tr " " "\n" | sort -V | head -n 1)" != "$1"; }
+function version_le() { test "$(echo "$@" | tr " " "\n" | sort -V | head -n 1)" == "$1"; }
+function version_lt() { test "$(echo "$@" | tr " " "\n" | sort -rV | head -n 1)" != "$1"; }
+function version_ge() { test "$(echo "$@" | tr " " "\n" | sort -rV | head -n 1)" == "$1"; }
 
 export PATH=$PATH:$PWD
 export WORKDIR=$PWD
@@ -16,25 +25,88 @@ INSTALLER=$1
 OUTPUT=$2
 SCRIPT="$(mktemp /tmp/tmp.XXXXXXXXX)"
 
+case $BUILD_ARCH in
+    unix)
+        SELECTEDPACKAGES=gcc_64
+        ;;
+    arm*|x86*)
+        if version_ge $QT_VERSION_DIR 5.14 ; then
+            SELECTEDPACKAGES=android
+        else
+            case $BUILD_ARCH in
+                arm*)
+                    SELECTEDPACKAGES=android_armv7
+                    ;;
+                x86*)
+                    SELECTEDPACKAGES=android_x86
+                    ;;
+            esac
+        fi
+        ;;
+    *)
+        echo "Aach[$BUILD_ARCH] don't suppoert"
+        ;;
+esac
+
 cat <<EOF > $SCRIPT
 function Controller() {
+    installer.autoRejectMessageBoxes();
     installer.installationFinished.connect(function() {
         gui.clickButton(buttons.NextButton);
     });
 }
 
+function log() {
+    var msg = ["QTCI: "].concat([].slice.call(arguments));
+
+    console.log(msg.join(" "));
+}
+
 Controller.prototype.WelcomePageCallback = function() {
-    gui.clickButton(buttons.NextButton);
+    gui.clickButton(buttons.NextButton, 3000);
 }
 
 Controller.prototype.CredentialsPageCallback = function() {
+    var login = installer.environmentVariable("QT_USER");
+    var password = installer.environmentVariable("QT_PASSWORD");
+    if( login === "" || password === "" ) {
+        console.log("No credentials provided - could stuck here forever");
+        gui.clickButton(buttons.CommitButton);
+    }
+    var widget = gui.currentPageWidget();
+    widget.loginWidget.EmailLineEdit.setText(login);
+    widget.loginWidget.PasswordLineEdit.setText(password);
     gui.clickButton(buttons.CommitButton);
 }
 
 Controller.prototype.ComponentSelectionPageCallback = function() {
+    var components = installer.components();
+    log("Available components: " + components.length);
+    for (var i = 0 ; i < components.length ;i++) {
+        log(components[i].name);
+    }
+    log("Select components");
+    function trim(str) {
+        return str.replace(/^ +/,"").replace(/ *$/,"");
+    }
     var widget = gui.currentPageWidget();
-
-    // widget.deselectAll();
+    widget.deselectAll();
+    var packages = trim("$SELECTEDPACKAGES").split(",");
+    if (packages.length > 0 && packages[0] !== "") {
+        
+        for (var i in packages) {
+            var pkg = trim(packages[i]);
+            for (var i = 0 ; i < components.length ;i++) {
+                if(components[i].name.indexOf(pkg) != -1)
+                {
+                    log("Select " + components[i].name);
+                    widget.selectComponent(trim(components[i].name));
+                }
+            }
+        }
+    } else {
+        log("Use default component list");
+    }
 
     gui.clickButton(buttons.NextButton);
 }
@@ -81,4 +153,7 @@ Controller.prototype.FinishedPageCallback = function() {
 EOF
 
 chmod u+x $1
-$1 --script $SCRIPT
+#显示log
+$1 -v --script $SCRIPT
+#不显示log
+#$1 --script $SCRIPT
